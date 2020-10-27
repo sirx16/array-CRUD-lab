350496370cc49972ed8ff701aa945457c5da735b
sirx16 <atonuslperry@gmail.com>
2020-10-27 10:46:29 -0600

    commit tree f70916e58d871dea8379e890862b305fbe175977

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,27 @@
 def create_an_empty_array
-  
+    [] 
 end
-
-def create_an_array
-  
-end
-
-def add_element_to_end_of_array(array, element)
-  
-end
-
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
+ def create_an_array 
+ ["Earth" , "Water" , "Wind" , "Fire"]
+ end
+ def add_element_to_end_of_array (array, element)
+     create_an_array.push("arrays!")
+ end
+ def add_element_to_start_of_array (array, element)
+    create_an_array.unshift("wow")
+ end
+ def remove_element_from_end_of_array(array)
+   array.pop
+ end
+ def remove_element_from_start_of_array(array)
+ array.shift
+ end
 def retrieve_element_from_index(array, index_number)
-  
+array[2]
 end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
+ def retrieve_first_element_from_array(array)
+    array[0]
+    end
+ def retrieve_last_element_from_array(array)
+ array.last 
 end
